b41a364820b9a9414d31d0677f8f89ee765e00f2
asalmeidarj <asalmeidarj@gmail.com>
2021-06-26 19:34:07 -0300

    Criando arquivos base para conexao com banco de dados

---
+++ conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Tranbalhando com Banco de dados
+ * com PHP Data Object
+ * 
+ * PHP version 8.0.7
+ *
+ * @category Sqlite
+ * @package  PDO
+ * @author   asalmeidarj <asalmeidarj@gmail.com>
+ * @license  GPL 3.0
+ * @link     https://github.com/asalmeidarj
+ */
+
+ 
+
+$pdo = new PDO('sqlite:banco.sqlite');
