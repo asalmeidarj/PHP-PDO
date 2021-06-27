46edd5ab463e1c5e3d2fa46563ac04c9e864343e
asalmeidarj <asalmeidarj@gmail.com>
2021-06-27 02:07:50 -0300

    Fazendo consulta com o método fetch mais WHILE

---
+++ fetch.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Select students in Table
+ * 
+ * PHP version 8.0.7
+ *
+ * @category Sqlite
+ * @package  PDO
+ * @author   asalmeidarj <asalmeidarj@gmail.com>
+ * @license  GPL 3.0
+ * @link     https://github.com/asalmeidarj
+ */
+
+ require_once 'vendor/autoload.php';
+
+
+
+// Connecting Data Base
+$dataBasePath = __DIR__ . '/banco.sqlite'; 
+$pdo = new PDO('sqlite:' . $dataBasePath);
+
+
+// Select students
+$statement = $pdo->query('SELECT * FROM students WHERE id = 3;');
+
+// Show Students
+while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
+    echo $studentData['name'] . PHP_EOL;
+}
+
+/*
+    NOTE: Removing 'WHERE id = 3' from the query method,
+          it would display all the names in the table.
+          When we pass the return value of the 'fetch' method
+          inside a WHILE an iteration occurs.
+          
+          Exemplo: 
+                                
+              $statement = $pdo->query('SELECT * FROM students;');
+
+              while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
+                  echo $studentData['name'] . PHP_EOL;
+              }
+*/
