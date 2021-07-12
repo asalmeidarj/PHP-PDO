3197b0ffdfdb30c3b70bae7930fa80904eb8d47e
asalmeidarj <asalmeidarj@gmail.com>
2021-07-11 22:08:05 -0300

    criando tabela phones

--- conexao.php
+++ conexao.php
@@ -3,7 +3,7 @@
 /**
  * Tranbalhando com Banco de dados
  * com PHP Data Object
- * 
+ *
  * PHP version 8.0.7
  *
  * @category Sqlite
@@ -11,19 +11,28 @@
  * @author   asalmeidarj <asalmeidarj@gmail.com>
  * @license  GPL 3.0
  * @link     https://github.com/asalmeidarj
+ *
  */
 
-
-$pathSqlite = __DIR__ . '/banco.sqlite'; 
+$pathSqlite = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $pathSqlite);
 
 echo 'Conectei';
 
 //// Criando uma tabela
 $pdo->exec(
-    'CREATE TABLE students (
+    'CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
         birth_date TEXT
-        );'
+    );
+        
+    CREATE TABLE IF NOT EXISTS phones (
+        id INTEGER PRIMARY KEY,
+        area_code TEXT,
+        number TEXT,
+        student_id INTEGER,
+        FOREIGN KEY(student_id) REFERENCES students(id)
+    );
+    '
 );
